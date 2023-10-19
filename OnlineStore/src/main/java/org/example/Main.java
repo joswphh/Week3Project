5aca20b0eec47a233d7e40cdf03c4f8a6eeb9c9e
nadia <nadiaandu7@gmail.com>
2023-10-19 11:43:02 -0500

    nadia

--- OnlineStore/src/main/java/org/example/Main.java
+++ OnlineStore/src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("nadia")
+        System.out.println("Hello I am nadia")
     }
         System.out.println("Hello Adam!");
 
