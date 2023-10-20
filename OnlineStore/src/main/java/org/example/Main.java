f30fb00e7bee8aee78c5f776e2af5df7e394ae4e
Chris <chrisgai01@icloud.com>
2023-10-20 10:54:26 -0500

    welcometoourstore

--- OnlineStore/src/main/java/org/example/Main.java
+++ OnlineStore/src/main/java/org/example/Main.java
@@ -1,8 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        boolean validInput = false;
+
+        while (!validInput) {
+            System.out.println("Welcome to Better Buy!");
+            System.out.println("1) List Products");
+            System.out.println("2) Add Products and Remove");
+            System.out.println("3) Search For Product");
+            System.out.println("4) Display Cart");
+            System.out.println("5) Exit Better Buy");
+            Scanner scanner = new Scanner(System.in);
+            int userInput = scanner.nextInt();
+
+            switch(userInput){
+
+            }
+        }
 
     }
-
 }
